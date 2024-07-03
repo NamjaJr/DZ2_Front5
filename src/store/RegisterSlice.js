2f95fd44a5f7f4f21fe32a8043da0ab1cff83d5b
NamjaJr <hanruslan144@gmail.com>
2024-07-03 21:16:00 +0600

    test

--- src/store/RegisterSlice.js
+++ src/store/RegisterSlice.js
@@ -2,25 +2,27 @@ import { configureStore, createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
     isRegistered: false
-};
+}
 
 const registerSlice = createSlice({
     name: 'registration',
     initialState,
     reducers: {
         register: (state) => {
-            state.isRegistered = true;
-        },
-    },
-});
+            state.isRegistered = true
+        }
+    }
+})
 
-export const { register } = registerSlice.actions
+
 
 const store = configureStore({
     reducer: {
         registration: registerSlice.reducer
     },
-});
+})
 
+export const { register } = registerSlice.actions
 export default store
+
 
